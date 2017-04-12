023e6ed94e02a2a54f539cfea67b1e910e6aed5a
Jon Eric Escobedo <jonericescobedo@gmail.com>
2017-04-12 10:36:19 -0700

    Complete all ES6 functionalities to this repo

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,18 @@
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Grub {
+  constructor() {
+    super();
+
+    this.age = 10;
+    this.job = 'make honey';
+    this.color = 'yellow';
+    this.honeyPot = 0;
+  }
+
+  makeHoney() {
+    this.honeyPot++;
+  }
+
+  giveHoney() {
+    this.honeyPot--;
+  }
 };
